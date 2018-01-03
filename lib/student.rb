4e8f2416e7c7ff71771ea829347591b9dca5beb1
Juliana Fogg <julianafogg@gmail.com>
2018-01-03 16:45:22 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,31 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
+  #hooks
   def initialize(student_hash)
+    student_hash.each |key,value| self.send("#{key}="),value)
+    @@all << self
     
   end
 
+  #constructor
+
   def self.create_from_collection(students_array)
-    
+    student = Student.new
+    properties= self.scrape_index_page(index_url)
+
+
   end
 
   def add_student_attributes(attributes_hash)
-    
+
   end
 
+  #class method ,class getter
   def self.all
-    
+    @@all
   end
 end
-
