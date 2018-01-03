e8ea2793ab8f8688cb829c284ffedfe79b5ae145
Juliana Fogg <julianafogg@gmail.com>
2018-01-03 17:00:41 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,7 +6,7 @@ class Student
 
   #hooks
   def initialize(student_hash)
-    student_hash.each {|key, value| self.send(("#{key}="), value)}
+    student_hash.each do |key, value| self.send(("#{key}="), value)
     @@all << self
 
   end
